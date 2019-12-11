f69489339cc8b2389cce2f673a25d88e42b83576
Ryan Liong <ryanliongjf@gmail.com>
2019-12-11 15:52:56 +0800

    Working API with React

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'static#index'
+  namespace :items, defaults: {format:'json' } do
+    get 'items', to: 'items#index'
+  end
+
   resources :items 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
