fff82725c0dd58c0cb496772fe392690e02b0c2d
Ryan Liong <ryanliongjf@gmail.com>
2019-12-13 12:27:46 +0800

    Working React Front End
    React Front End works, basic Database implemented

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     get 'items', to: 'items#index'
   end
 
-  resources :items 
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
